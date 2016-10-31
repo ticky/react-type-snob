7f9969d73b95ec3513c7912662f95d156a797380
Jessica Stokes <hello@jessicastokes.net>
2016-10-31 21:27:29 +0800

    📝 Update Readme, improve getDisplayName

--- src/get-display-name.js
+++ src/get-display-name.js
@@ -1,4 +1,4 @@
-export default function getDisplayName(type, props = {}) {
+export default function getDisplayName(type, props) {
   let displayName = 'Unknown Element';
 
   if (type.displayName) {
@@ -7,10 +7,12 @@ export default function getDisplayName(type, props = {}) {
   } else if (typeof type === 'string') {
     // if it's a string, we'll use it straight up, then append some identifying stuff
     displayName = type;
-    if (typeof props.id === 'string') {
-      displayName = [displayName, props.id].join('#');
-    } else if (typeof props.className === 'string') {
-      displayName = [displayName, ...props.className.split(' ')].join('.');
+    if (props) {
+      if (typeof props.id === 'string') {
+        displayName = `${displayName}#${props.id}`;
+      } else if (typeof props.className === 'string') {
+        displayName = [displayName, ...props.className.split(' ')].join('.');
+      }
     }
   } else if (typeof type === 'function' && type.name) {
     // if it's a function, let's use the name if it's not blank!
