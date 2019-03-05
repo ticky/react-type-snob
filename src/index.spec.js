cbe3c34da0ba942683bbe1629d43eb1d2e1e39f7
Jessica Stokes <hello@jessicastokes.net>
2019-03-05 15:58:35 -0800

    🆕 (Finally) update to Babel 7.x
    Also correct spec suite to work with updated Jest plugins

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,12 +2,12 @@
 import typeSnob from './index';
 import { Children, createElement as realCreateElement, isValidElement } from 'react';
 
-const fakeGetDisplayName = jest.fn(() => 'component-name-here');
-jest.mock('./get-display-name', () => (...args) => fakeGetDisplayName(...args));
-const fakeVerifyReact = jest.fn();
-jest.mock('./verify-react', () => (...args) => fakeVerifyReact(...args));
-const infractionReporter = jest.fn();
-jest.mock('./console-reporter', () => (...args) => infractionReporter(...args));
+const mockGetDisplayName = jest.fn(() => 'component-name-here');
+jest.mock('./get-display-name', () => (...args) => mockGetDisplayName(...args));
+const mockVerifyReact = jest.fn();
+jest.mock('./verify-react', () => (...args) => mockVerifyReact(...args));
+const mockInfractionReporter = jest.fn();
+jest.mock('./console-reporter', () => (...args) => mockInfractionReporter(...args));
 
 describe('Type Snob', () => {
   const createElement = jest.fn(() => ({
@@ -25,10 +25,10 @@ describe('Type Snob', () => {
 
   it('binds to supplied React object, and doesn\'t call it until called', () => {
     typeSnob(FakeReact);
-    expect(fakeVerifyReact).toHaveBeenCalledWith(FakeReact);
+    expect(mockVerifyReact).toHaveBeenCalledWith(FakeReact);
     expect(FakeReact.createElement).not.toBe(createElement);
     expect(createElement).not.toHaveBeenCalled();
-    expect(fakeGetDisplayName).not.toHaveBeenCalled();
+    expect(mockGetDisplayName).not.toHaveBeenCalled();
   });
 
   describe('typeSnobCreateElement', () => {
@@ -39,7 +39,7 @@ describe('Type Snob', () => {
         }).not.toThrow();
         expect(createElement).toHaveBeenCalledTimes(1);
         expect(createElement).toHaveBeenCalledWith('span', null, 'hello there!');
-        expect(fakeGetDisplayName).not.toHaveBeenCalled();
+        expect(mockGetDisplayName).not.toHaveBeenCalled();
       });
 
       it('with multiple argument children', () => {
@@ -48,7 +48,7 @@ describe('Type Snob', () => {
         }).not.toThrow();
         expect(createElement).toHaveBeenCalledTimes(1);
         expect(createElement).toHaveBeenCalledWith('div', null, 'hello there ', 'you');
-        expect(fakeGetDisplayName).not.toHaveBeenCalled();
+        expect(mockGetDisplayName).not.toHaveBeenCalled();
       });
 
       it('with prop children', () => {
@@ -57,7 +57,7 @@ describe('Type Snob', () => {
         }).not.toThrow();
         expect(createElement).toHaveBeenCalledTimes(1);
         expect(createElement).toHaveBeenCalledWith('div', { children: ['hello there ', 'you'] });
-        expect(fakeGetDisplayName).not.toHaveBeenCalled();
+        expect(mockGetDisplayName).not.toHaveBeenCalled();
       });
 
       it('with no children', () => {
@@ -66,7 +66,7 @@ describe('Type Snob', () => {
         }).not.toThrow();
         expect(createElement).toHaveBeenCalledTimes(1);
         expect(createElement).toHaveBeenCalledWith('div', null);
-        expect(fakeGetDisplayName).not.toHaveBeenCalled();
+        expect(mockGetDisplayName).not.toHaveBeenCalled();
       });
     });
 
@@ -75,24 +75,24 @@ describe('Type Snob', () => {
         expect(() => {
           FakeReact.createElement('span', null, 'it\'s a shame you didn\'t use a “curly quote” here');
         }).not.toThrow();
-        expect(infractionReporter).toHaveBeenCalledTimes(1);
-        expect(infractionReporter.mock.calls).toMatchSnapshot();
+        expect(mockInfractionReporter).toHaveBeenCalledTimes(1);
+        expect(mockInfractionReporter.mock.calls).toMatchSnapshot();
       });
 
       it('ugly double quotes', () => {
         expect(() => {
           FakeReact.createElement('span', null, 'it’s a shame you didn’t use a "curly quote" here');
         }).not.toThrow();
-        expect(infractionReporter).toHaveBeenCalledTimes(1);
-        expect(infractionReporter.mock.calls).toMatchSnapshot();
+        expect(mockInfractionReporter).toHaveBeenCalledTimes(1);
+        expect(mockInfractionReporter.mock.calls).toMatchSnapshot();
       });
 
       it('full-stops as ellipses', () => {
         expect(() => {
           FakeReact.createElement('span', null, 'this typography sucks...');
         }).not.toThrow();
-        expect(infractionReporter).toHaveBeenCalledTimes(1);
-        expect(infractionReporter.mock.calls).toMatchSnapshot();
+        expect(mockInfractionReporter).toHaveBeenCalledTimes(1);
+        expect(mockInfractionReporter.mock.calls).toMatchSnapshot();
       });
 
       it('full-stops as ellipses', () => {
@@ -100,8 +100,8 @@ describe('Type Snob', () => {
           FakeReact.createElement('span', null, 'this typography *really* sucks ...');
           FakeReact.createElement('span', null, 'this typography *really* sucks, too …');
         }).not.toThrow();
-        expect(infractionReporter).toHaveBeenCalledTimes(2);
-        expect(infractionReporter.mock.calls).toMatchSnapshot();
+        expect(mockInfractionReporter).toHaveBeenCalledTimes(2);
+        expect(mockInfractionReporter.mock.calls).toMatchSnapshot();
       });
     });
 
@@ -109,15 +109,15 @@ describe('Type Snob', () => {
       expect(() => {
         FakeReact.createElement('span', null, realCreateElement('span', null, 'hi there'));
       }).not.toThrow();
-      expect(infractionReporter).not.toHaveBeenCalled();
+      expect(mockInfractionReporter).not.toHaveBeenCalled();
     });
 
     it('gracefully handles multiple suggestions', () => {
       expect(() => {
         FakeReact.createElement('span', null, 'you didn\'t make very good use of Unicode here ...');
       }).not.toThrow();
-      expect(infractionReporter).toHaveBeenCalledTimes(1);
-      expect(infractionReporter.mock.calls).toMatchSnapshot();
+      expect(mockInfractionReporter).toHaveBeenCalledTimes(1);
+      expect(mockInfractionReporter.mock.calls).toMatchSnapshot();
     });
   });
 });
